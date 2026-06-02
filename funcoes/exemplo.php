0823e04b9d43281b78657c3abd619c70d2cad90b
Rafaelcostadelima <rafita.limagames2@gmail.com>
2026-06-02 08:41:29 -0300

    Adição de um arquivo de função

---
+++ funcoes/exemplo.php
@@ -0,0 +1,10 @@
+<?php
+
+echo 'Função é uma funcionalidade das linguagens de programação. <br>Ela serve para reduzir o código, para não ficar tão extenso, repetitivo e difícil de se fazer uma manutenção. <br>A seguir, vamos declarar uma função com um parâmetro "nome".<br><br><code>function saudacoes ($nome){<br>echo "Olá, $nome!";<br>}</code>';
+
+function saudacoes($nome){
+    echo "Olá, $nome!";
+}
+
+echo '<br><br>Após essa declaração, vamos chamar a função e colocar uma string com o valor de "Rafael".<br><code>saudades("Rafael");</code><br><br>Em resumo, função é isso, mas nos aprofundaremos mais a frente.<br><br>';
+saudacoes("Rafael"); 
