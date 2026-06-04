ec0d2442d3205e114adba50d072d6c2be02d001e
Rafito3080 <rafita.limagames@gmail.com>
2026-06-04 15:39:43 -0300

    Comentário de nada no index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
+    echo "Olá, ser da terra! Este repositório serve inteiramente voltado para PHP, sua semântica, funções, funcionalidades entre outros. <br>";
     echo "Atual versão do PHP: ". phpversion();
-?>
