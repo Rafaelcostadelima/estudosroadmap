73a2135fb13a6a4678e9abbe3263ab44ec033d93
Rafaelcostadelima <rafita.limagames2@gmail.com>
2026-06-09 08:46:55 -0300

    Resolução de todos os exercícios propostos pelo Gemini.

--- exercicios/exercicio07.php
+++ exercicios/exercicio07.php
@@ -1,2 +1,26 @@
 <?php
 
+$categoriaPrincipal = [
+    "nome" => "Moda",
+    "quantidade" => 10,
+    "subcategoria" => [
+        "nome" => "Masculino",
+        "quantidade" => 5,
+        "subcategoria" => [
+            "nome" => "Calçados",
+            "quantidade" => 2,
+            "subcategoria" => null // Fim da árvore
+        ]
+    ]
+];
+
+function somarProdutos($categoria){
+    $somaProdutos = 0;
+    $somaProdutos = $somaProdutos + $categoria['quantidade'];
+    if ($categoria['subcategoria'] != null){
+        $somaProdutos += somarProdutos($categoria['subcategoria']);
+    }
+    return $somaProdutos;
+}
+
+echo somarProdutos($categoriaPrincipal);
