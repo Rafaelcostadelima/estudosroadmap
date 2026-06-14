61675082f3c4be0de766cff7b133398f9b77335e
Rafito3080 <rafita.limagames@gmail.com>
2026-06-13 21:28:01 -0300

    Resolução do exercício 8 propostos pelo Gemini

--- exercicios/exercicio07.php
+++ exercicios/exercicio07.php
@@ -1,2 +1,12 @@
 <?php
+//Declarando a função anônima de aplicar a taxa
+$aplicarTaxa = fn($soma) => $soma * 1.1;
+function calcularTotalCarrinho($listaDePrecos, $aplicarTaxa){
+    $soma = 0;
+    foreach($listaDePrecos as $preco){
+        $soma += $preco;
+    }
+    return $aplicarTaxa($soma);
+}
 
+echo calcularTotalCarrinho([50, 40, 10], $aplicarTaxa);
